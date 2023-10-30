d54c26f19f334421cd6dbebf1905a358e53c5340
FrederikRudHansen <frha0002rstud.kea.dk>
2023-10-30 11:49:43 +0100

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        StartMenu startMenu = new StartMenu(); // Opret en instans af StartMenu
+        StartMenu startMenu = new StartMenu();
 
-        // Kald main metoden i StartMenu
         startMenu.menu();
     }
 }
