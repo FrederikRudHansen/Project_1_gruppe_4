dd8a9eb823d768cc4f84ba7bf13bc0d88ba5439b
FrederikRudHansen <frha0002rstud.kea.dk>
2023-11-02 10:34:16 +0100

    Grundprojektet

--- src/StartMenu.java
+++ src/StartMenu.java
@@ -11,6 +11,8 @@ public class StartMenu {
         int totalbeløb = 0; // Reseveret beløb der ikke er blevet betalt endnu
         int balance = 100; // Virksomhedens balance
         int totalBal = balance + totalbeløb;
+        int Shampoo = 50;
+        int Balsam = 25;
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
@@ -29,20 +31,24 @@ public class StartMenu {
             if (valg == 1) {
                 // betalings metoden
                 System.out.println("betal her");
-                System.out.println("tast 1 for godkend");
-                System.out.println("tast 2 for tilbage");
+                System.out.println("tast 1: for at gennemføre betaling");
+                System.out.println("tast 2: for at tilføje et proukt til kunden");
+                System.out.println("tast 3: for tilbage");
                 System.out.print("tast her: ");
                 int betal = scanner.nextInt();
                 if (betal == 1) {
-                    System.out.println("Skriv Kundens navn");
+                    System.out.println("Kunder ⇩ ");
                     for (Kunde kunde : kundeListe) {
                         System.out.println("Navn: " + kunde.navn + ", Tider: " + kunde.tider + ", Beløb: " + kunde.beløb);
 
                     }
+                    System.out.println("Skriv Kundens navn: ");
                     String navn = scanner.next();
                     for (int i = 0; i < kundeListe.size(); i++) {
                         Kunde kunde = kundeListe.get(i);
                         if (kunde.navn.equals(navn)) { //hvis man skriver kundens præcise navn. bliver den slettet fra arraylisten
+
+
                             kundeListe.remove(i);
                             totalbeløb -= kunde.beløb;
                             totalBal -= kunde.beløb;
@@ -56,95 +62,129 @@ public class StartMenu {
 
                         }
                     }
-                }
-            } else if (valg == 2) {
-                System.out.println("tast 1: Vis aftaler");
-                System.out.println("tast 2: Opret ny tid");
-                System.out.println("tast 3: Slet aftaler");
-                System.out.println("tast 4: Opret Feriedage");
-                System.out.println("tast 5: se ferier");
-                System.out.println("tast 6: Tilbage");
-                int fris = scanner.nextInt();
-                if (fris == 1) {
-                    System.out.println("Alle aftaler ⇩");
+                } else if (betal == 2) {
+                    System.out.println(" Kunder ⇩");
                     for (Kunde kunde : kundeListe) {
+
                         System.out.println("Navn: " + kunde.navn + ", Tider: " + kunde.tider + ", Beløb: " + kunde.beløb);
 
                     }
-
-
-                }
-
-                if (fris == 2) {
-                    System.out.println("Feriedage ⇩");
-                    for (Ferie ferie : FerieDage) {
-                        System.out.println("Navn: " + ferie.navn + " Dato: "+ ferie.dato);
-
-                    }
-                    System.out.println("Andre aftaler ⇩");
-                    for (Kunde kunde : kundeListe) {
-                        System.out.println("Navn: " + kunde.navn + ", Tider: " + kunde.tider + ", Beløb: " + kunde.beløb);
-
-                    }
-
-                    System.out.print("Indtast navn: ");
+                    System.out.print("Skriv kundens navn: ");
                     String navn = scanner.next();
-
-                    System.out.print("Indtast dato og tid feks. (24/12/2023-11:00): ");
-                    String tid = scanner.next();
-
-                    System.out.print("Indtast beløb: ");
-                    int beløb = scanner.nextInt();
-
-                    Kunde nyKunde = new Kunde(navn, tid, beløb); // Opretter en ny kunde og tilføjer den til kundeListe
-                    kundeListe.add(nyKunde);
-
-                    totalbeløb += beløb;
-                    totalBal += beløb;
-
-                    System.out.println("Ny kunde oprettet");
-
-                } else if (fris == 3) {
-
-                    System.out.println("Indtast navn på kunden, du vil slette: ");
-
-                    for (Kunde kunde : kundeListe) {
-                        System.out.println("Navn: " + kunde.navn + ", Tider: " + kunde.tider + ", Beløb: " + kunde.beløb);
-
-                    }
-                    String navn = scanner.next();
-
                     for (int i = 0; i < kundeListe.size(); i++) {
                         Kunde kunde = kundeListe.get(i);
-                        if (kunde.navn.equals(navn)) { //hvis man skriver kundens præcise navn. bliver den slettet fra arraylisten
-                            kundeListe.remove(i);
-                            totalbeløb -= kunde.beløb;
-                            totalBal -= kunde.beløb;
+                        if (kunde.navn.equals(navn)) { //hvis man skriver kundens præcise navn. bliver den slettet fra
 
-                            System.out.println("Kunde med navn "
-                                    + navn + " er blevet slettet.");
-                            break; // breaker når den har fundet kunden
+                            while (true) {
+                                System.out.println("Tast 1: Tilføj shampoo ");
+                                System.out.println("Tast 2: Tilføj balsam ");
+                                System.out.println("Tast 3: Tilbage");
+                                int pro = scanner.nextInt();
+                                if (pro == 1) {
+                                    kunde.beløb += Shampoo;
+                                    totalbeløb += Shampoo;
+                                    totalBal -= Shampoo;
+                                }
+                                if (pro == 2) {
+                                    kunde.beløb += Balsam;
+                                    totalbeløb += Balsam;
+                                    totalBal -= Balsam;
+                                } else {
+                                    break;
+                                }
 
+                            } // while slut
+                        }
+                    }
+                }
+                } else if (valg == 2) {
+                    System.out.println("tast 1: Vis aftaler");
+                    System.out.println("tast 2: Opret ny tid");
+                    System.out.println("tast 3: Slet aftaler");
+                    System.out.println("tast 4: Opret Feriedage");
+                    System.out.println("tast 5: se ferier");
+                    System.out.println("tast 6: Tilbage");
+                    int fris = scanner.nextInt();
+                    if (fris == 1) {
+                        System.out.println("Alle aftaler ⇩");
+                        for (Kunde kunde : kundeListe) {
+                            System.out.println("Navn: " + kunde.navn + ", Tider: " + kunde.tider + ", Beløb: " + kunde.beløb);
+
+                        }
+
+
+                    }
+
+                    if (fris == 2) {
+                        System.out.println("Feriedage ⇩");
+                        for (Ferie ferie : FerieDage) {
+                            System.out.println("Navn: " + ferie.navn + " Dato: " + ferie.dato);
+
+                        }
+                        System.out.println("Andre aftaler ⇩");
+                        for (Kunde kunde : kundeListe) {
+                            System.out.println("Navn: " + kunde.navn + ", Tider: " + kunde.tider + ", Beløb: " + kunde.beløb);
+
+                        }
+
+                        System.out.print("Indtast navn: ");
+                        String navn = scanner.next();
+
+                        System.out.print("Indtast dato og tid feks. (24/12/2023-11:00): ");
+                        String tid = scanner.next();
+
+                        System.out.print("Indtast beløb: ");
+                        int beløb = scanner.nextInt();
+
+                        Kunde nyKunde = new Kunde(navn, tid, beløb); // Opretter en ny kunde og tilføjer den til kundeListe
+                        kundeListe.add(nyKunde);
+
+                        totalbeløb += beløb;
+                        totalBal += beløb;
+
+                        System.out.println("Ny kunde oprettet");
+
+                    } else if (fris == 3) {
+
+                        System.out.println("Indtast navn på kunden, du vil slette: ");
+
+                        for (Kunde kunde : kundeListe) {
+                            System.out.println("Navn: " + kunde.navn + ", Tider: " + kunde.tider + ", Beløb: " + kunde.beløb);
+
+                        }
+                        String navn = scanner.next();
+
+                        for (int i = 0; i < kundeListe.size(); i++) {
+                            Kunde kunde = kundeListe.get(i);
+                            if (kunde.navn.equals(navn)) { //hvis man skriver kundens præcise navn. bliver den slettet fra arraylisten
+                                kundeListe.remove(i);
+                                totalbeløb -= kunde.beløb;
+                                totalBal -= kunde.beløb;
+
+                                System.out.println("Kunde med navn "
+                                        + navn + " er blevet slettet.");
+                                break; // breaker når den har fundet kunden
+
+
+                            }
+                        }
+                    } else if (fris == 4) {
+                        System.out.println("indtast navn: ");
+                        String navn = scanner.next();
+
+                        System.out.println("indtast slut dato og start dato feks. (12/10/2023-20/10/2023:");
+                        String dato = scanner.next();
+
+                        Ferie NyFerie = new Ferie(navn, dato);
+                        FerieDage.add(NyFerie);
+
+                    } else if (fris == 5) {
+                        System.out.println("Feriedage ⇩");
+                        for (Ferie ferie : FerieDage) {
+                            System.out.println("Navn: " + ferie.navn + " Dato: " + ferie.dato);
 
                         }
                     }
-                } else if (fris == 4){
-                    System.out.println("indtast navn: ");
-                    String navn = scanner.next();
-
-                    System.out.println("indtast slut dato og start dato feks. (12/10/2023-20/10/2023:");
-                    String dato = scanner.next();
-
-                    Ferie NyFerie = new Ferie(navn,dato);
-                    FerieDage.add(NyFerie);
-
-                } else if(fris==5){
-                    System.out.println("Feriedage ⇩");
-                    for (Ferie ferie : FerieDage) {
-                        System.out.println("Navn: " + ferie.navn + " Dato: "+ ferie.dato);
-
-                    }
-                }
 
                 } else if (valg == 3) {
                     System.out.println("tast 1: vis aftaler");
@@ -160,7 +200,7 @@ public class StartMenu {
                     } else if (sekr == 2) {
                         System.out.println("Feriedage ⇩");
                         for (Ferie ferie : FerieDage) {
-                            System.out.println("Navn: " + ferie.navn + " Dato: "+ ferie.dato);
+                            System.out.println("Navn: " + ferie.navn + " Dato: " + ferie.dato);
 
                         }
                         System.out.println("Andre aftaler ⇩");
@@ -214,37 +254,40 @@ public class StartMenu {
                     System.out.println("tast 2: Økonomi");
                     System.out.println("tast 3: Tilbage");
                     int rev = scanner.nextInt();
-                scanner.nextLine(); //fjerner "newline" i linje 179 så "scanner.nextLine();" virker i linje 194
+                    scanner.nextLine(); //fjerner "newline" i linje 179 så "scanner.nextLine();" virker i linje 194
 
-                if (rev == 1) {
-                    for (Kunde kunde : kundeListe) {
-                        System.out.println("Navn: " + kunde.navn + ", Tider: " + kunde.tider + ", Beløb: " + kunde.beløb);
+                    if (rev == 1) {
+                        for (Kunde kunde : kundeListe) {
+                            System.out.println("Navn: " + kunde.navn + ", Tider: " + kunde.tider + ", Beløb: " + kunde.beløb);
 
-                    }
-                    System.out.println("Total " + totalbeløb);
-
-                }
-                if (rev == 2) {
-                    String kode = "hairyharry"; //koden til login
-                    System.out.println("Skriv koden for at få adgang til økonomi filerne");
-
-                    String kodeTilføjet = scanner.nextLine();
-
-                    if (kodeTilføjet.equals(kode)) {
-                        System.out.println("Kode bekræftet - Velkommen!");
-                        System.out.println("Balance: " + balance);
-                        System.out.println("Reseveret betaling: " + totalbeløb);
-                        System.out.println("Balance + Reseveret betaling: " + totalBal);
-                    } else {
-                        System.out.println("Forkert kode - Prøv igen!");
-
+                        }
+                        System.out.println("Total " + totalbeløb);
 
                     }
-                    // Log in
+                    if (rev == 2) {
+                        String kode = "hairyharry"; //koden til login
+                        System.out.println("Skriv koden for at få adgang til økonomi filerne");
 
-                }
+                        String kodeTilføjet = scanner.nextLine();
 
-            } //while slut
+                        if (kodeTilføjet.equals(kode)) {
+                            System.out.println("Kode bekræftet - Velkommen!");
+                            System.out.println("Balance: " + balance);
+                            System.out.println("Reseveret betaling: " + totalbeløb);
+                            System.out.println("Balance + Reseveret betaling: " + totalBal);
+                            System.out.println("Tast 1: tilbage");
+                            String til = scanner.next();
+                        } else {
+                            System.out.println("Forkert kode - Prøv igen!");
+
+
+                        }
+                        // Log in
+
+                    }
+
+                } //while slut
+            }
         }
     }
-}
+
