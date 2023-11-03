2317672dda13f861b7e0725816338813e2bd02c5
FrederikRudHansen <frha0002rstud.kea.dk>
2023-11-03 10:44:09 +0100

    Final

--- src/BookTid.java
+++ src/BookTid.java
@@ -2,27 +2,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class BookTid {
-    public static void Book() {
-        ArrayList<Kunde> kundeListe = new ArrayList<>();
 
-        Kunde kunde1 = new Kunde("Bodil", "11:00",200);
-
-        Kunde kunde2 = new Kunde("Hans","12:00",200);
-
-        kundeListe.add(kunde1);
-        kundeListe.add(kunde2);
-        int totalbeløb = 0;
-        for (Kunde kunde : kundeListe){
-            totalbeløb += kunde.beløb;
-        }
-
-
-        for (Kunde kunde : kundeListe) {
-            System.out.println("Navn: " + kunde.navn + ", Tider: " + kunde.tider + ", Beløb: " + kunde.beløb);
-
-        }
-        System.out.println("Resveret beløb "+totalbeløb);
-    }
 }
 
 class Kunde{
